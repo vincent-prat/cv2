23c4b0ba5e8dccdcf00db5ee44ffb1679f8b1ed0
Vincent PRAT <prat.vincent.romain@gmail.com>
2022-07-12 16:27:06 +0200

    menu en attente

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementsByTagName("nav");
+    if (x.className === "nav") {
+      x.className += "responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
